dc229521cf9f6fe282a208ebf5cbfedca12d394c
Endy WindAflame <dev.windaflame@gmail.com>
2018-04-08 19:46:54 +0200

    add contracts

---
+++ src/app/app.routes.contracts.ts
@@ -0,0 +1,5 @@
+export const AppRoutesContracts = {
+    // Generic
+    EMPTY: '',
+    OTHERS: '**'
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from "@angular/router";
 import { LayoutComponent } from "./layout/layout.component";
+import { AppRoutesContracts as Path } from "./app.routes.contracts";
 
 export const AppRoutes: Routes = [
     //{ path: '', component: redirectTo: '/', pathMatch: 'full' }
-    { path: '', component: LayoutComponent },
+    { path: Path.EMPTY, component: LayoutComponent },
     //{ path: '**', component: PageNotFoundComponent }
-    { path: '**', redirectTo: '', pathMatch: 'full' }
+    { path: Path.OTHERS , redirectTo: Path.EMPTY, pathMatch: 'full' }
 ]
